1bc07edc77a18d93073cc7db5a5fbea25b0cb74a
JackLovel <gogkatsu@outlook.com>
2020-11-02 12:01:20 +0800

    add qt creator config

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 set -x PATH $PATH /opt/Qt5.14.2/5.14.2/gcc_64/bin
 set -x QTDIR /opt/Qt5.14.2 $QTDIR
 set -x LD_LIBRARY_PATH $QTDIR/lib
-# alias qt="cd /opt/Qt5.14.2/Tools/QtCreator/bin && ./qtcreator.sh"
+alias qt="cd /opt/Qt5.14.2/Tools/QtCreator/bin && ./qtcreator.sh";
+
 set -x CMAKE_PREFIX_PATH /usr/lib/x86_64-linux-gnu/cmake 
